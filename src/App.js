ab263401555fa2a75195400a79a87442e31bc404
tony <wangh9947@gmail.com>
2023-11-16 13:23:18 +0000

    updating 2 selector, convertDistance

--- src/App.js
+++ src/App.js
@@ -39,10 +39,20 @@ import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [distance, setDistance] = useState(0);
   const [userInput, setUserInput] = useState(0);
-  const [conversionType, setConversionType] = useState("KMtoMiles");
+  const [selector1, setSelector1] = useState("Miles");
+  const [selector2, setSelector2] = useState("Miles");
   const [convertedValue, setConvertedValue] = useState(0);
+
+  const convertDistance = () => {
+    if (selector1 === selector2) {
+      setConvertedValue(userInput); // No conversion needed
+    } else if (selector1 === "Miles" && selector2 === "KM") {
+      setConvertedValue(userInput * 1.6); // Convert Miles to KM
+    } else if (selector1 === "KM" && selector2 === "Miles") {
+      setConvertedValue(userInput * 0.62); // Convert KM to Miles
+    }
+  };
 
   return (
     <div className="App">
@@ -52,7 +62,10 @@ function App() {
       <body>
         <h3>Please enter the distance</h3>
 
-        <select name="milesOrKm" id="unit">
+        <select
+          value={selector1}
+          onChange={(e) => setSelector1(e.target.value)}
+        >
           <option value="Miles">Miles</option>
           <option value="KM">KM</option>
         </select>
@@ -62,11 +75,21 @@ function App() {
           onChange={(e) => setUserInput(e.target.value)}
           value={userInput}
         />
-        <select name="milesOrKm" id="unit">
+
+        <select
+          value={selector2}
+          onChange={(e) => setSelector2(e.target.value)}
+        >
           <option value="Miles">Miles</option>
           <option value="KM">KM</option>
         </select>
-        <button>Convert</button>
+
+        <button onClick={convertDistance}>Convert</button>
+
+        <h2>
+          {convertedValue} {selector2}
+        </h2>
+        <h2></h2>
       </body>
     </div>
   );
