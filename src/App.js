14a82f026bf1bbb9f70f30fb5934f0d37899741e
tony <wangh9947@gmail.com>
2023-11-16 13:09:45 +0000

    2 selects

--- src/App.js
+++ src/App.js
@@ -1,38 +1,75 @@
+// import "./App.css";
+// import { useState } from "react";
+
+// function App() {
+//   const [miles, setMiles] = useState(0);
+//   const [userInput, setUserInput] = useState(0);
+//   const [kmValue, setKmValue] = useState(0);
+//   return (
+//     <div className="App">
+//       <header>
+//         <h1>KM to Miles Converter</h1>
+//       </header>
+//       <body>
+//         <h3>Please enter the distance in KM</h3>
+//         <input type="dropbox"></input>
+//         <input
+//           onChange={(e) => {
+//             setUserInput(e.target.value);
+//           }}
+//           value={userInput}
+//         ></input>
+//         <button
+//           onClick={() => {
+//             setMiles(userInput * 0.62);
+//             setKmValue(userInput);
+//           }}
+//         >
+//           Convert
+//         </button>
+//       </body>
+//       <h2>
+//         {kmValue} KM = {miles} miles
+//       </h2>
+//     </div>
+//   );
+// }
+// export default App;
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-  const [miles, setMiles] = useState(0);
+  const [distance, setDistance] = useState(0);
   const [userInput, setUserInput] = useState(0);
-  const [kmValue, setKmValue] = useState(0);
+  const [conversionType, setConversionType] = useState("KMtoMiles");
+  const [convertedValue, setConvertedValue] = useState(0);
+
   return (
     <div className="App">
       <header>
-        <h1>KM to Miles Converter</h1>
+        <h1>Distance Converter</h1>
       </header>
       <body>
-        <h3>Please enter the distance in KM</h3>
+        <h3>Please enter the distance</h3>
+
+        <select name="milesOrKm" id="unit">
+          <option value="Miles">Miles</option>
+          <option value="KM">KM</option>
+        </select>
+
         <input
-          onChange={(e) => {
-            setUserInput(e.target.value);
-            setKmValue(e.target.value);
-            setMiles(e.target.value * 0.62);
-          }}
+          type="number"
+          onChange={(e) => setUserInput(e.target.value)}
           value={userInput}
-        ></input>
-        <button
-          onClick={() => {
-            setMiles(userInput * 0.62);
-            setKmValue(userInput);
-          }}
-        >
-          Convert
-        </button>
+        />
+        <select name="milesOrKm" id="unit">
+          <option value="Miles">Miles</option>
+          <option value="KM">KM</option>
+        </select>
+        <button>Convert</button>
       </body>
-      <h2>
-        {kmValue} KM = {miles} miles
-      </h2>
     </div>
   );
 }
+
 export default App;
